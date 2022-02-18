776e43795395c31c50b3524ebde507eb6640c289
Padmavathi <99352066+PadmavathiDataScientist@users.noreply.github.com>
2022-02-18 15:14:04 +0530

    Add files via upload

---
+++ using_constructor.py
@@ -0,0 +1,16 @@
+class A:
+    def __init__(self):
+        self.x=78
+        self.y=78
+    def f(a,b):
+        print("ghim")
+        if(a==b):
+            print("x equal to y")
+        else:
+            print("x not equal to y")
+        return "hiiii"
+a=A()
+print(a.x)
+print(a.f(1))
+
+            
