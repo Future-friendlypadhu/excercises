60fdda10f6f66e95ac1c92f3b43afc228adbb505
Padmavathi <99352066+PadmavathiDataScientist@users.noreply.github.com>
2022-02-18 15:09:28 +0530

    Add files via upload

---
+++ class_program.py
@@ -0,0 +1,12 @@
+class A:
+    x=78 #class var
+    def __init__(self): #constructor
+        self.a=98      #instance var
+        self.b=75
+    def f(): 
+        x=5    
+        print(x)
+    f()
+a=A()
+print(a.a, a.b)
+    
